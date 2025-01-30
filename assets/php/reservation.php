895094c284634c5a0ba3e794a23cbe80c1445f74
Akonaay <ambrosesulley4@gmail.com>
2025-01-31 02:04:32 +0300

    UI - Home, Safari&Tour

--- assets/php/reservation.php
+++ assets/php/reservation.php
@@ -1,13 +1,17 @@
 <?php
 //get data from form  
 
-$name = $_POST['name'];
+$name = $_POST['fname'];
 $email = $_POST['email'];
-$service = $_POST['service'];
+$phone = $_POST['phone'];
+$duration = $_POST['duration'];
+$tour_type = $_POST['tour_type'];
+$message = $_POST['message'];
+
 $to = "reservations@cosmostravel.co.tz";
 // reservations@cosmostravel.co.tz
-$subject = "Mail From website | Reservation Form";
-$txt = "Name = " . $name . "\r\n  Email = " . $email . "\r\n Service =" . $service;
+$subject = "Mail From Website | Safari & Tours Request";
+$txt = "Name = " . $name . "\r\n  Email = " . $email . "\r\n Phone =" . $phone . "\r\n Tour Duration =" . $duration . "\r\n Tour Type =" . $tour_type . "\r\n Message =" . $message;
 $headers = "From: noreply@cosmostravel.co.tz";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
